1c49d91bc0acf8a563546be4fb48a242de2f6513
Nero Argento <drakonobor@gmail.com>
2024-03-09 12:14:43 +0300

    fixed search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "wikipedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.get_article, name="get_article")
+    path("wiki/<str:title>", views.get_article, name="get_article"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import render
 from django import forms
 
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search")
 
 from . import util
 import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
 def get_article(request, title):
     result = util.get_entry(title)
-    if request.method == "POST":
-        if result is None:
-            return render(request, "encyclopedia/error.html", {
-                "title": title
+    if result is None:
+        return render(request, "encyclopedia/error.html", {
+                "title": title,
+                "form": SearchForm()
             })
-        return render(request, "encyclopedia/article.html", {
+    return render(request, "encyclopedia/article.html", {
             "title": title,
-            "entry": markdown2.markdown(result)
+            "entry": markdown2.markdown(result),
+            "form": SearchForm()
         })
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["search"]
+            return get_article(request, title)
+    else:
+        form = SearchForm()
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+        "form": form
+    })
+
